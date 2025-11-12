82d2e9ae7a553c6e7a4389956ef3e5f0b7abfadc
Nugraha Panca Wibisana <nugrahapancawibisana@gmail.com>
2025-11-12 16:22:37 +0700

    feat: adds feature fetch user

--- js/fetch-user.js
+++ js/fetch-user.js
@@ -1,16 +1,15 @@
 export async function fetchUser() {
-    try {
-        const url = 'https://jsonplaceholder.typicode.com/users'
+  try {
+    const url = 'https://jsonplaceholder.typicode.com/users';
 
-        const res = await fetch(url);
+    const res = await fetch(url);
 
-        if (!res.ok) {
-            throw new Error(res.status);
-            return;
-        }
-
-        return res.json();
-    } catch (error) {
-        console.log(error);
+    if (!res.ok) {
+      throw new Error(res.status);
     }
+
+    console.log(res.json());
+  } catch (error) {
+    console.log(error);
+  }
 }
